e6a2b83e4e41e567234d41ddcee1f43973698589
vlad_vm <non@non.no>
2016-02-29 21:56:14 +0300

    запуск jsp скрипта из нужной папки в иерархии

--- src/ReportServer.java
+++ src/ReportServer.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -15,30 +20,53 @@ import java.io.IOException;
 public class ReportServer {
 
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private URI getWebRootResourceUri() throws FileNotFoundException, URISyntaxException {
+        URL indexUri = ReportServer.class.getResource("/webapp");
+        if (indexUri == null) {
+            throw new FileNotFoundException("Unable to find resource " + "/webapp");
+        }
+        return indexUri.toURI();
+    }
 
+    public void Init() throws FileNotFoundException, URISyntaxException {
         System.setProperty("org.apache.jasper.compiler.disablejsr199", "false");
 
-        String jetty_home = System.getProperty("jetty.home",".");
+        //String jetty_home = System.getProperty("jetty.home","D:/projects/ReportServer");
+
 
         Server server = new Server(8080);
 
         WebAppContext context = new WebAppContext();
 
         context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
-                             ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/.*taglibs.*\\.jar$");
+                ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/.*taglibs.*\\.jar$");
 
-        context.setResourceBase(jetty_home+"/src/webapp");
+        try {
+            URI baseUri = getWebRootResourceUri();
+            context.setResourceBase(baseUri.toASCIIString());
+        }
+        catch (URISyntaxException e)
+        {
+
+        }
+
         context.setContextPath("/");
 
-        //context.setAttribute("javax.servlet.context.tempdir",scratchDir);
-
-        context.setClassLoader(new URLClassLoader(new URL[0], ReportServer.class.getClassLoader()));
+        context.setClassLoader( new URLClassLoader(new URL[0], ReportServer.class.getClassLoader() ) );
         context.setParentLoaderPriority(true);
 
         // Add Default Servlet (must be named "default")
         ServletHolder holderDefault = new ServletHolder("default", DefaultServlet.class);
-        holderDefault.setInitParameter("resourceBase", jetty_home+"/src/webapp");
+
+        try {
+            URI baseUri = getWebRootResourceUri();
+            holderDefault.setInitParameter("resourceBase", baseUri.toASCIIString());
+        }
+        catch  (URISyntaxException e)
+        {
+
+        }
+
         holderDefault.setInitParameter("dirAllowed", "true");
         context.addServlet(holderDefault, "/");
 
@@ -55,18 +83,21 @@ public class ReportServer {
 
         // Add Application Servlets
         ServletHolder tabSynchronizeHolder = new ServletHolder("TabSynchronize", TabSynchronize.class);
-        tabSynchronizeHolder.setInitParameter("resourceBase", jetty_home+"/src/webapp");
+        try {
+            URI baseUri = getWebRootResourceUri();
+            tabSynchronizeHolder.setInitParameter( "resourceBase", baseUri.toASCIIString() );
+        }
+        catch  (URISyntaxException e) {
+
+        }
+
         tabSynchronizeHolder.setInitParameter("dirAllowed", "true");
         context.addServlet(tabSynchronizeHolder, "/TabSynchronize");
 
         ServletHolder exampleJspHolder = new ServletHolder();
-        tabSynchronizeHolder.setInitParameter("resourceBase", jetty_home+"/src/webapp/WEB-INF/jsps");
-        exampleJspHolder.setForcedPath(jetty_home+"/src/webapp/WEB-INF/jsps/example.jsp");
-        //exampleJspHolder.setInitParameter("resourceBase", jetty_home+"/src/webapp/WEB-INF/jsps");
-        //exampleJspHolder.setInitParameter("dirAllowed", "true");
+        exampleJspHolder.setForcedPath("/WEB-INF/jsps/example.jsp");
         exampleJspHolder.setName("example.jsp");
         context.addServlet(exampleJspHolder, "/example");
-
 
         server.setHandler(context);
 
@@ -74,11 +105,23 @@ public class ReportServer {
             server.start();
             server.join();
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             System.out.println(e);
         }
+    }
 
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ReportServer server = new ReportServer();
+
+        try {
+            server.Init();
+        }
+        catch(Exception e) {
+
+        }
     }
 
 
